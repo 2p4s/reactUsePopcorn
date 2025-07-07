2a65833e4a9e8c4e715143528082dc93e6acc681
Marc Ahmed <marcahmed@googlemail.com>
2025-07-07 09:50:39 +0100

    data fetch cleanup (aborted requests)

--- src/App.js
+++ src/App.js
@@ -85,13 +85,16 @@ export default function App() {
   //runs it after first mount
   useEffect(
     function () {
+      const controller = new AbortController(); //browser control API for cancelling requests
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           //clear any errors
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal } //pass as param
           );
 
           //check for error
@@ -102,6 +105,7 @@ export default function App() {
 
           setMovies(data.Search);
           // console.log(data.Search);
+          setError(""); //clears the aborted fetch requests
           setIsLoading(false);
         } catch (err) {
           console.error(err.message);
@@ -115,6 +119,10 @@ export default function App() {
         setError("");
       }
       fetchMovies();
+      //Cleanup function, runs when component is unloaded / removed, prevent multiple runs of this func
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   ); //empty array means run "onMount" ie. first run, but setting to query, it will refresh on each change of that state
